c46a33dc2e17f57b6b7c1133645501091f259dea
proesd <ericksuryadinata@gmail.com>
2017-12-14 06:21:48 +0700

    fix Dashboard folder

--- Dashboard/views/welcome_message.php
+++ Dashboard/views/welcome_message.php
@@ -68,7 +68,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter! Algoritma Genetika</h1>
+	<h1>Welcome to CodeIgniter! Dashboard</h1>
+	<a href="<?php echo base_url().'AlgoritmaGenetika'?>">asdasd</a>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
--- index.php
+++ index.php
@@ -114,7 +114,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'AlgoritmaGenetika';
+	$application_folder = 'Dashboard';
 
 /*
  *---------------------------------------------------------------

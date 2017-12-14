243de65cf03e67f776f1488fcee34a304fcea09c
proesd <ericksuryadinata@gmail.com>
2017-12-15 05:59:53 +0700

    penambahan vendor

---
+++ AlgoritmaGenetika_APP/views/App/index.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Algoritma Genetika</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css')?>">
+</head>
+<body>
+
+
+	<script src="<?php echo base_url('assets/vendor/jquery/jquery-3.2.1.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/vendor/popper/dist/umd/popper.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.min.js');?>"></script>
+</body>
+</html>
